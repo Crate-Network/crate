0cb4a8b433ae18f38892ec6aa7be526cf35e84eb
Christian van der Loo <chris.vanderloo@yahoo.com>
2022-05-31 17:59:51 +0000

    Added FileEventListeners

--- src/models/FileModel.ts
+++ src/models/FileModel.ts
@@ -1,4 +1,10 @@
 export type FileModel = {
   id: string
+  cid: string
   name: string
+}
+
+export type FileEventListeners = {
+  onRenameRequest?: (finish: (newName: string) => void) => void
+  onDelete?: () => void
 }
