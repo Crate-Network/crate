ba327c892273b550ba04a6032b99cf3b0d48deb8
Christian van der Loo <chris.vanderloo@yahoo.com>
2022-05-31 11:22:14 +0000

    Tweaks to toolbar shadows

--- src/pages/files/Toolbar.tsx
+++ src/pages/files/Toolbar.tsx
@@ -99,13 +99,16 @@ export function ViewBar({ viewMode, setViewMode }) {
       >
         View Mode
       </label>
-      <div className="inline-flex" id="view-mode">
+      <div
+        className="inline-flex shadow-sm rounded overflow-hidden"
+        id="view-mode"
+      >
         <button
           className={`${
             viewMode === ViewMode.GRID
               ? "bg-slate-300 dark:bg-slate-600"
               : "bg-slate-100 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-700"
-          }  text-slate-800 dark:text-slate-200 py-2 px-4 rounded-l`}
+          }  text-slate-800 dark:text-slate-200 py-2 px-4`}
           onClick={() => setViewMode(ViewMode.GRID)}
         >
           <FontAwesomeIcon icon={faGrip} />
@@ -115,7 +118,7 @@ export function ViewBar({ viewMode, setViewMode }) {
             viewMode === ViewMode.LIST
               ? "bg-slate-300 dark:bg-slate-600"
               : "bg-slate-100 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-700"
-          }  text-slate-800 dark:text-slate-200 py-2 px-4 rounded-r`}
+          }  text-slate-800 dark:text-slate-200 py-2 px-4`}
           onClick={() => setViewMode(ViewMode.LIST)}
         >
           <FontAwesomeIcon icon={faBars} />
