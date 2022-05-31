921a2e341104cf40ba1d56296caa4deb6f533cf1
Christian van der Loo <chris.vanderloo@yahoo.com>
2022-05-31 10:55:23 +0000

    model upgrades

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,3 +1,5 @@
+import { FileModel } from "./FileModel"
+
 type UserModel = {
   devices: string[]
   firstName: string
@@ -6,6 +8,21 @@ type UserModel = {
   recoveryKey: string
   signedDataKey: Record<string, string>
   uses2FA: boolean
+  files: FileModel
 }
 
+const defaultUserModel = {
+  devices: [],
+  firstName: "",
+  lastName: "",
+  organization: "",
+  recoveryKey: "",
+  signedDataKey: {},
+  uses2FA: false,
+  files: {
+    id: "",
+  },
+}
+
+export { defaultUserModel }
 export default UserModel
