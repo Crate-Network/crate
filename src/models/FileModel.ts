a1ed2cfc9304f5c2281fd7be1f01be1213d426a0
Christian van der Loo <chris.vanderloo@yahoo.com>
2022-06-01 13:00:56 +0000

    documented filemodel

--- src/models/FileModel.ts
+++ src/models/FileModel.ts
@@ -1,3 +1,5 @@
+import { UnixFS } from "ipfs-unixfs"
+
 export enum FileType {
   RAW,
   FILE,
@@ -5,12 +7,28 @@ export enum FileType {
 }
 
 export type FileModel = {
+  // unique ID of this particular file
   id: string
+  // CID from the contents of the file
   cid: string
+  // name without extension
   name: string
+  // full file name
   fullName: string
-  extension: string
+  // extension (if exists)
+  extension?: string
+  // type of file
   type: FileType
+  // whether the contents of this file are encrypted
+  encrypted: boolean
+  // the encryption key for this file
+  encKey: string
+  // the size of the file
+  size: number
+  // if folder, size of all children, otherwise cumulativeSize == size
+  cumulativeSize: number
+  // the unixFS node for the file (if downloaded)
+  unixFS?: UnixFS
 }
 
 export type FileEventListeners = {
