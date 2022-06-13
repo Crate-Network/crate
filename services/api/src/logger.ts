f374912da47ca58c6df24b614f8a7ecc03b7cf15
Christian van der Loo <chris.vanderloo@yahoo.com>
2022-06-13 18:33:47 -0400

    fixed up logging

--- services/api/src/logger.ts
+++ services/api/src/logger.ts
@@ -4,7 +4,18 @@ const logger = winston.createLogger({
   level: "info",
   format: winston.format.json(),
   defaultMeta: { service: "user-service" },
-  transports: [new winston.transports.Console({ stderrLevels: ["error"] })],
+  transports: [
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" }),
+  ],
 });
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
 
 export default logger;
