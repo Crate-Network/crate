7dd517d288c19ba329ec6be57276af226f231830
Christian van der Loo <chris.vanderloo@yahoo.com>
2022-06-13 23:33:03 -0400

    small restructure of authenticate

--- web/src/pages/Authenticate.tsx
+++ web/src/pages/Authenticate.tsx
@@ -2,12 +2,7 @@ import Button from "components/Button"
 import FormBox from "components/FormBox"
 import FormInput from "components/FormInput"
 import AppleLogo from "assets/signin-apple-logo.svg"
-import {
-  getRedirectResult,
-  sendSignInLinkToEmail,
-  signInWithRedirect,
-  UserCredential,
-} from "firebase/auth"
+import { sendSignInLinkToEmail, signInWithRedirect } from "firebase/auth"
 import { Link, route } from "preact-router"
 import { useState } from "preact/hooks"
 import { actionCodeSettings, auth, providers } from "vendor/firebase"
@@ -18,6 +13,88 @@ export enum AuthenticateType {
   LOGIN = "Login",
 }
 
+type SignInBtnProps = {
+  providerText: string
+  disabled: boolean
+  handleProvider: (cb: () => Promise<void>) => void
+}
+
+function SignInWithGoogle({
+  handleProvider,
+  disabled,
+  providerText,
+}: SignInBtnProps) {
+  const useGoogle = handleProvider(async () => {
+    await signInWithRedirect(auth, providers.google)
+  })
+
+  return (
+    <Button
+      id="google-signin"
+      disabled={disabled}
+      className="text-black p-2 w-full h-8 overflow-hidden bg-gray-100 mt-4 flex flex-row justify-between hover:shadow-md transition-shadow align-middle items-center"
+      onClick={useGoogle}
+    >
+      <img
+        className="h-8 p-2"
+        alt="Google sign-in"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"
+      />
+      {providerText} with Google
+      <span />
+    </Button>
+  )
+}
+
+function SignInWithGithub({
+  handleProvider,
+  disabled,
+  providerText,
+}: SignInBtnProps) {
+  const useGitHub = handleProvider(async () => {
+    await signInWithRedirect(auth, providers.github)
+  })
+
+  return (
+    <Button
+      id="github-signin"
+      disabled={disabled}
+      className="text-black p-2 w-full h-8 overflow-hidden bg-gray-100 mt-4 flex flex-row justify-between hover:shadow-md transition-shadow align-middle items-center"
+      onClick={useGitHub}
+    >
+      <img
+        className="h-8 p-2"
+        alt="GitHub sign-in"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Font_Awesome_5_brands_github.svg/2560px-Font_Awesome_5_brands_github.svg.png"
+      />
+      {providerText} with GitHub
+      <span />
+    </Button>
+  )
+}
+
+function SignInWithApple({
+  handleProvider,
+  disabled,
+  providerText,
+}: SignInBtnProps) {
+  const useApple = handleProvider(async () => {
+    await signInWithRedirect(auth, providers.apple)
+  })
+  return (
+    <Button
+      id="apple-signin"
+      disabled={disabled}
+      className="text-white p-2 w-full h-9 overflow-hidden bg-black mt-4 flex flex-row justify-between hover:shadow-md transition-shadow align-middle items-center"
+      onClick={useApple}
+    >
+      <AppleLogo className="h-8 mt-0.5" alt="Apple sign-in" />
+      {providerText} with Apple
+      <span />
+    </Button>
+  )
+}
+
 export default function Authenticate({ type }: { type: AuthenticateType }) {
   const loggedIn = useUserStore((state) => state.signedIn)
 
@@ -26,7 +103,7 @@ export default function Authenticate({ type }: { type: AuthenticateType }) {
   const [disableInputs, setDisableInputs] = useState(false)
   const providerText = type === AuthenticateType.LOGIN ? "Sign in" : "Sign up"
 
-  const handleProvider = (providerCb) => async () => {
+  const handleProvider = (providerCb: () => void) => async () => {
     setDisableInputs(true)
     try {
       await providerCb()
@@ -44,18 +121,6 @@ export default function Authenticate({ type }: { type: AuthenticateType }) {
     await sendSignInLinkToEmail(auth, email, actionCodeSettings)
     window.localStorage.setItem("emailForSignIn", email)
     setEmailSent(true)
-  })
-
-  const useGoogle = handleProvider(async () => {
-    await signInWithRedirect(auth, providers.google)
-  })
-
-  const useGitHub = handleProvider(async () => {
-    await signInWithRedirect(auth, providers.github)
-  })
-
-  const useApple = handleProvider(async () => {
-    await signInWithRedirect(auth, providers.apple)
   })
 
   if (emailSent)
@@ -87,45 +152,21 @@ export default function Authenticate({ type }: { type: AuthenticateType }) {
       >
         {providerText} with Email
       </Button>
-      {/* <div className="h-px w-full bg-gray-300 dark:bg-gray-600" /> */}
-      <Button
-        id="github-signin"
+      <SignInWithApple
         disabled={disableInputs}
-        className="text-white p-2 w-full h-9 overflow-hidden bg-black mt-4 flex flex-row justify-between hover:shadow-md transition-shadow align-middle items-center"
-        onClick={useApple}
-      >
-        <AppleLogo className="h-8 mt-0.5" alt="Apple sign-in" />
-        {providerText} with Apple
-        <span />
-      </Button>
-      <Button
-        id="google-signin"
+        providerText={providerText}
+        handleProvider={handleProvider}
+      />
+      {/* <SignInWithGoogle
         disabled={disableInputs}
-        className="text-black p-2 w-full h-8 hidden overflow-hidden bg-gray-100 mt-4 flex flex-row justify-between hover:shadow-md transition-shadow align-middle items-center"
-        onClick={useGoogle}
-      >
-        <img
-          className="h-8 p-2"
-          alt="Google sign-in"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"
-        />
-        {providerText} with Google
-        <span />
-      </Button>
-      <Button
-        id="github-signin"
+        providerText={providerText}
+        handleProvider={handleProvider}
+      />
+      <SignInWithGitHub
         disabled={disableInputs}
-        className="text-black p-2 w-full h-8 hidden overflow-hidden bg-gray-100 mt-4 flex flex-row justify-between hover:shadow-md transition-shadow align-middle items-center"
-        onClick={useGitHub}
-      >
-        <img
-          className="h-8 p-2"
-          alt="GitHub sign-in"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Font_Awesome_5_brands_github.svg/2560px-Font_Awesome_5_brands_github.svg.png"
-        />
-        {providerText} with GitHub
-        <span />
-      </Button>
+        providerText={providerText}
+        handleProvider={handleProvider}
+      /> */}
       <div className="mt-4 mb-1 h-px w-full bg-gray-300 dark:bg-gray-600" />
       <span className="text-sm text-gray-700 dark:text-gray-300">
         {type === AuthenticateType.LOGIN ? (
