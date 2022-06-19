cb68a9232780eaa10cc2aa0c2f73d02b1b19fed5
Christian van der Loo <chris.vanderloo@yahoo.com>
2022-06-18 21:26:23 -0400

    feat: moved common

--- packages/common/src/FileModel.ts
+++ packages/common/src/FileModel.ts
@@ -1,12 +1,9 @@
 export enum FileType {
-  RAW = 0,
-  FILE = 1,
-  DIRECTORY = 2,
+  FILE = "file",
+  DIRECTORY = "directory",
 }
 
 type BaseFileModel = {
-  // unique ID of this particular file
-  readonly id: string
   // CID from the contents of the file
   readonly cid: string
   // name without extension
@@ -23,22 +20,16 @@ type BaseFileModel = {
   encKey: string
   // the size of the file
   readonly size: number
-  // if folder, size of all children, otherwise cumulativeSize == size
-  readonly cumulativeSize: number
 }
 
 export type FileModel = (
   | {
       type: FileType.FILE
-      contents?: Uint8Array
     }
   | {
       type: FileType.DIRECTORY
-      children: FileModel[]
-    }
-  | {
-      type: FileType.RAW
-      contents?: Uint8Array
+      links: string[]
+      cumulativeSize: number
     }
 ) &
   BaseFileModel
