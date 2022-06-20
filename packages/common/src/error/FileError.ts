7d234c7297a98b0c4dfe63f2b3d38e9880b4b745
Christian van der Loo <chris.vanderloo@yahoo.com>
2022-06-20 01:09:34 -0400

    fix: tweaked FileError

--- packages/common/src/error/FileError.ts
+++ packages/common/src/error/FileError.ts
@@ -1,17 +1,17 @@
-import { FileModel } from "@crate/common"
+import { FileModel } from "../model"
 
 export enum FileErrorType {
   EXISTS = "File with name already exists",
   FILE_INVALID = "Invalid usage, expected to be of type FileType.FOLDER",
   FOLDER_INVALID = "Invalid usage, expected to be of type FileType.FILE",
+  NO_DATA = "Data field expected but not found on data block.",
 }
 
 export class FileError extends Error {
   readonly file: FileModel
-  constructor(m: FileErrorType, f: FileModel) {
+  constructor(m: FileErrorType) {
     super(m)
     Object.setPrototypeOf(this, new.target.prototype) // restore prototype chain
     this.name = FileError.name
-    this.file = f
   }
 }
