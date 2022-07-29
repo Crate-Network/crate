9ad857d393cb9ac0d168e97fa41ae44bc919f1a4
Christian van der Loo <chris.vanderloo@yahoo.com>
2022-07-29 13:47:16 -0400

    fix: main.js in docker image for server

--- apps/server/src/clients/files.ts
+++ apps/server/src/clients/files.ts
@@ -1,6 +1,11 @@
 import { create } from "@crate/files-client"
+import logger from "../logger"
 
-export default create({
+const client = create({
   url: process.env["IPFS_CLIENT_URL"],
   timeout: 30000,
 })
+
+logger.info("IPFS Client Started")
+
+export default client
