766804c0dbe0dddc90c41f932dbf009f40bbb845
Christian van der Loo <chris.vanderloo@yahoo.com>
2022-08-16 21:04:02 -0500

    build: repatched ipfs-utils, added postinstall, externalized electron

--- packages/server/src/types/express/index.d.ts
+++ packages/server/src/types/express/index.d.ts
@@ -2,9 +2,10 @@ import express from "express"
 import { DecodedIdToken } from "firebase-admin/auth"
 
 declare global {
-  namespace Express {
-    interface Request {
+  declare module Express {
+    export interface Request {
       token?: DecodedIdToken
+      user: string
     }
   }
 }
