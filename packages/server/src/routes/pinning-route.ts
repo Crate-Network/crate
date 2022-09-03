370fa345de754e6575cadc6034b1a2ab29c0fdac
Christian van der Loo <chris.vanderloo@yahoo.com>
2022-09-03 17:47:56 -0400

    fix: added client logging

--- packages/server/src/routes/pinning-route.ts
+++ packages/server/src/routes/pinning-route.ts
@@ -1,6 +1,7 @@
 import { Pin, PinStatus, Status } from "@crate/types"
 import { Router } from "express"
 import { uuid } from "uuidv4"
+import fcClient from "../clients/filecoin"
 
 const router = Router()
 
